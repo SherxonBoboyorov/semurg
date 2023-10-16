eba99b9e265385caef7be299d9723266d43d7346
Sherxon <116145014+SherxonBoboyorov@users.noreply.github.com>
2023-10-16 12:42:44 +0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\SliderController;
+use UniSharp\Laravel\LaravelFilemanager\Lfm;
+
+
+Auth::routes();
+
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware(['role:admin'])->prefix('dashboard')->group(static function () {
+    Route::get('/', [HomeController::class, 'index'])->name('homeAdmin');
+    Route::resources([
+        'slider' => SliderController::class,
+    ]);
+});
+
+
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+    ], function(){ 
+            //
+    });
+
+
+
+Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web', 'auth']], function () {
+    UniSharp\LaravelFilemanager\Lfm::routes();
+});
+
