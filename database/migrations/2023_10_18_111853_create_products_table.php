c8eb261fa321b72cebbdad212ba2fe392acfa717
Sherxon <116145014+SherxonBoboyorov@users.noreply.github.com>
2023-10-18 17:07:06 +0500

    some changes

---
+++ database/migrations/2023_10_18_111853_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('category_id')->nullable()->unsigned();
+            $table->foreign('category_id')
+            ->references('id')
+            ->on('categories')
+            ->onDelete('cascade');
+
+            $table->string('image');
+
+            $table->string('title_ru');
+            $table->string('title_uz');
+            $table->string('title_en');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
