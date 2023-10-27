195068ead5f2d4dab68b88cdd39964ad7b1af85c
Sherxon <116145014+SherxonBoboyorov@users.noreply.github.com>
2023-10-27 18:23:12 +0500

    some changes

--- resources/views/admin/poll/create.blade.php
+++ resources/views/admin/poll/create.blade.php
@@ -69,14 +69,70 @@
                         <h1>Answer</h1>
 
                         <div class="answers">
-                            <button id="add-answer">Add Answer</button>
+                            <button class="btn btn-primary" id="add-answer">Add Answer</button>
                         
                             <div class="answer-template" style="display: none;">
                                 <div class="answer">
-                                    <input type="text" name="answer_en" placeholder="Answer EN">
-                                    <input type="text" name="answer_ru" placeholder="Answer RU">
-                                    <input type="text" name="answer_uz" placeholder="Answer UZ">
-                                    <button class="remove-answer">-</button>
+
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <label for="answer_uz">Answer UZ</label>
+                                            <input type="text" id="answer_uz" class="form-control" name="answer_uz[]">
+                                            @if($errors->has('answer_uz'))
+                                                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                    {{ $errors->first('answer_uz') }}
+            
+                                                </div>
+                                            @endif
+                                        </div>
+                                    </div>
+            
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <label for="answer_ru">Answer RU</label>
+                                            <input type="text" id="answer_ru" class="form-control" name="answer_ru[]">
+                                            @if($errors->has('answer_ru'))
+                                                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                    {{ $errors->first('answer_ru') }}
+                                                </div>
+                                            @endif
+                                        </div>
+                                    </div>
+            
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <label for="answer_en">Answer EN</label>
+                                            <input type="text" id="answer_en" class="form-control" name="answer_en[]">
+                                            @if($errors->has('answer_en'))
+                                                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                    {{ $errors->first('answer_en') }}
+                                                </div>
+                                            @endif
+                                        </div>
+            
+                                    </div><br><samp></samp>
+
+                                    <div class="col-md-6">
+                                        <label class="form-label" for="order">Order</label>
+                                        <div class="input-group">
+                                            <input id="order" name="order" type="number" aria-label="Order"
+                                                aria-describedby="order" class="touchspin" value="1"/>
+                                        </div>
+                                    </div><br><br>
+
+
+                                 
+
+                                    <button style="width: 20px; border: 2px solid red" class="remove-answer">-</button>
                                 </div>
                             </div>
                         
@@ -99,8 +155,10 @@
 @section('custom_js')
 <script>
 
-   document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('add-answer').addEventListener('click', function() {
+  document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('add-answer').addEventListener('click', function(event) {
+        event.preventDefault(); 
+        
         var newAnswer = document.querySelector('.answer-template .answer').cloneNode(true);
         document.querySelector('.added-answers').appendChild(newAnswer);
     });
@@ -110,7 +168,8 @@
             event.target.closest('.answer').remove();
         }
     });
-});
+    
+  });
 
 </script>
 @endsection
