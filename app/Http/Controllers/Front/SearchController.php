273b2448cdbcca3bf0cc9e00ce2f03d472633c15
Boboyorov <sherxonbabayar@gmail.com>
2023-11-05 18:00:35 +0500

    last changes

--- app/Http/Controllers/Front/SearchController.php
+++ app/Http/Controllers/Front/SearchController.php
@@ -13,15 +13,36 @@ class SearchController extends Controller
 {
     public function search(Request $request)
     {
+
         if(strlen($request->phrase) < 0) {
             return redirect()->route('/');
         }
+        $news = Article::whereLike(['title_uz', 'title_ru', 'title_en'], $request->phrase)->get();
         $leaderships = Leadership::whereLike(['name_uz', 'name_ru', 'name_en'], $request->phrase)->get();
         $tenders = Tender::whereLike(['title_uz', 'title_ru', 'title_en'], $request->phrase)->get();
 
         return view('front.search', compact(
+            'news',
             'leaderships',
             'tenders'
         ));
+        // $search = request()->query('search');
+
+        // if($search) {
+        //     $news = Article::where('title_uz', 'title_ru', 'title_en', 'LIKE', "%{$search}%")->get();
+        //     $leaderships = Leadership::where('name_uz', 'name_ru', 'name_en', 'LIKE', "%{$search}%")->get();
+        //     $tenders = Tender::where('title_uz', 'title_ru', 'title_en', 'LIKE', "%{$search}%")->get(); 
+        // } else {
+        //     $news = Article::paginate(1);
+        //     $leaderships = Leadership::paginate(1);
+        //     $tenders = Tender::paginate(1);
+        // }
+    
+        // return view('front.search', compact(
+        //     'search',
+        //     'news',
+        //     'leaderships',
+        //     'tenders'
+        // ));
     }
 }
