e06d9293203d0c60be529fa7c015cffa700d40ba
Boboyorov <sherxonboboyorov@gmail.com>
2023-12-13 14:51:59 +0500

    lang chanfes

--- config/app.php
+++ config/app.php
@@ -215,7 +215,6 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
-        'Telegram'  => Telegram\Bot\Laravel\Facades\Telegram::class
     ])->toArray(),
 
 ];
